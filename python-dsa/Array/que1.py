9c505271d50cefc0bd450f3c4388be5a396382be
sourav sambit behera <78550945+souravsambitbehera@users.noreply.github.com>
2025-01-14 22:57:08 +0530

    Add files via upload

---
+++ python-dsa/Array/que1.py
@@ -0,0 +1,23 @@
+# Create a list of the first 10 even numbers.
+# even_numbers = []
+# for item in range(1,50):
+#     if item%2==0:
+#         even_numbers.append(item)
+#     if len(even_numbers) ==10:
+#         break
+# print(even_numbers)
+
+# even_numbers = list(range(2,21,2))
+
+
+
+
+even_numbers = []
+num = 2
+while len(even_numbers)<10:
+    even_numbers.append(num)
+    num +=2
+
+
+
+

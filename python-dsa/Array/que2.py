ad00e574826da51d70c0c0a0fb992a530f2be935
sourav sambit behera <78550945+souravsambitbehera@users.noreply.github.com>
2025-01-14 23:04:20 +0530

    find the sum of all elements in a list of integers

---
+++ python-dsa/Array/que2.py
@@ -0,0 +1,6 @@
+# Write a Python program to find the sum of all elements in a list of integers. Use a loop to iterate through the list.
+nums = [1,2,3,4]
+sum = 0
+for item in nums:
+    sum += item
+print(sum)
